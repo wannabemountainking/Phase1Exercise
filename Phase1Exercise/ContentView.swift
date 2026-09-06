21d150d3336f959771daf9e7c8ac2a03e9e8f5cd
wannabemountainking <kdy0096@gmail.com>
2026-09-06 22:14:19 +0900

    Initial Commit

---
+++ Phase1Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Phase1Exercise
+//
+//  Created by YoonieMac on 9/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Phase1Exercise/Phase1ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phase1ExerciseApp.swift
+//  Phase1Exercise
+//
+//  Created by YoonieMac on 9/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Phase1ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
